b1010fe6c4c2667298feb8b38f933fbba166dd39
Stewart Thomson <stewart.thomson@bemo.co>
2019-08-13 20:41:34 +0100

    Initial Commit

---
+++ MVVM-C Demo AppTests/MVVM_C_Demo_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_C_Demo_AppTests.swift
+//  MVVM-C Demo AppTests
+//
+//  Created by Stewart Thomson on 13/08/2019.
+//  Copyright © 2019 Stewart Thomson. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_C_Demo_App
+
+class MVVM_C_Demo_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
